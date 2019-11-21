dc6be24279b9b7dac76c3a8a36f870ef05d8b907
betamake <hukunlun@buaa.edu.cn>
2019-11-21 17:11:31 +0800

    2019-11-21

--- betamake_数组与字符串.py
+++ betamake_数组与字符串.py
@@ -18,4 +18,17 @@ def is_repeat(string):
 判断是否互为字符重排
 解析：1，这个问题假设字符串a与b,也就是说。a中的字符全排列后，是否存在b的可能。
       2，另外一种想法是a与b的字符是否全部相等
+      注意点：首先问清楚是否区分大小写
 """
+def is_equal(string_a,string_b):
+    a_length = len(string_a)
+    b_length = len(string_b)
+    if a_length!=b_length:
+        return False
+    step = 1
+    for i in range(a_length):
+        if string_a[i] in string_b:
+            step +=1
+    if step == a_length:
+        return True
+    return False
